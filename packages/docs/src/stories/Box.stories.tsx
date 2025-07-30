783976876b89cddc1b1b986e1523935444cdd7ae
Christian Chang Thomaz <christianchangthomaz@gmail.com>
2025-07-30 16:32:44 -0300

    Add Text Component

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,16 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { Box } from "@ignite-ui/react";
+import { Box, Text } from "@ignite-ui/react";
 import type { BoxProps } from "@ignite-ui/react";
 
 export default {
   title: "Surfaces/Box",
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Testando o elemento Box</span>
-      </>
-    ),
+    children: <Text>Testando o elemento Box</Text>,
   },
 } as Meta<BoxProps>;
 
