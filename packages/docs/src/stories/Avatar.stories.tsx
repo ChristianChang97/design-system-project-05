0530a424cc8406c1c440acb755be8a26288549d3
Christian Chang Thomaz <christianchangthomaz@gmail.com>
2025-07-30 23:06:10 -0300

    Add Components
    - Heading
    - Avatar
    - Button

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Avatar } from "@ignite-ui/react";
+import type { AvatarProps } from "@ignite-ui/react";
+
+export default {
+  title: "Data display/Avatar",
+  component: Avatar,
+  args: {
+    src: "https://avatars.githubusercontent.com/u/61232440?v=4",
+    alt: "Christian",
+  },
+} as Meta<AvatarProps>;
+
+export const Primary: StoryObj<AvatarProps> = {};
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+};
